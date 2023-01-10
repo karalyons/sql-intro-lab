222b34d864f7878d1510a1cde1a3a413d7dcd2bf
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-01-10 22:16:46 +0000

    formatting

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -2,7 +2,8 @@
 
 select sum(stats.hits) as 'Lifetime Hits'
 from  stats inner join players on stats.player_id = players.id
-where players.first_name = 'Barry' and players.last_name = 'Bonds';
+where players.first_name = 'Barry' 
+    and players.last_name = 'Bonds';
 
 -- Expected result:
 -- 2935
