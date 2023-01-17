f80068d96ff16b161faf2d257356220abdaa7a53
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-01-17 20:04:19 +0000

    update changes

---
+++ school-5-insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO students (
+    "first_name",
+    "last_name",
+    "email"
+) VALUES (
+    "Kara",
+    "Lyons",
+    "kara.e.lyons@mgail.com"
+)
---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+    UPDATE students
+    SET first_name = "Karl"
+    WHERE id = 1;
